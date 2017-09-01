388495f302e245f14c0a45448c088e5938be0322
cooperbuilt <evanncooper@gmail.com>
2017-09-01 07:06:02 -0400

    Fix date issues with Moment.js

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,29 +1,25 @@
 import axios from 'axios';
+import Moment from 'moment';
 
 export function getStories(desk, offset, search) {
   // Create a date object and roll it back 7 days
   // Then use that 7 day marker as the begin_date for article search
-  Date.prototype.yyyymmdd = function() {
-    var mm = (this.getMonth() + 1).toString();
-    var dd = (this.getDate()-7).toString();
-    return [this.getFullYear(), mm.length===2 ? '' : '0', mm, dd.length===2 ? '' : '0', dd].join('');
-  };
 
-  var date = new Date();
-  var lastWeekTodayString = date.yyyymmdd();
+  const lastWeek = Moment().subtract(7, 'days').format('YYYYMMDD');
+  console.log(lastWeek);
 
   if (desk === '') {
-    return axios.get(`https://api.nytimes.com/svc/search/v2/articlesearch.json?fq=section:Today's Headlines&sort=newest&page=${offset}&begin_date=${lastWeekTodayString}&api-key=a8457610b68381085a3fff38d6a36337:6:74255139`)
+    return axios.get(`https://api.nytimes.com/svc/search/v2/articlesearch.json?fq=section:Today's Headlines&sort=newest&page=${offset}&begin_date=${lastWeek}&api-key=a8457610b68381085a3fff38d6a36337:6:74255139`)
     .then(response => response.data);
   }
 
   if (arguments.length === 2) {
-    return axios.get(`https://api.nytimes.com/svc/search/v2/articlesearch.json?fq=news_desk:${desk}&sort=newest&page=${offset}&begin_date=${lastWeekTodayString}&api-key=a8457610b68381085a3fff38d6a36337:6:74255139`)
+    return axios.get(`https://api.nytimes.com/svc/search/v2/articlesearch.json?fq=news_desk:${desk}&sort=newest&page=${offset}&begin_date=${lastWeek}&api-key=a8457610b68381085a3fff38d6a36337:6:74255139`)
       .then(response => response.data);
   }
 
   else {
-    return axios.get(`https://api.nytimes.com/svc/search/v2/articlesearch.json?q=${search}&sort=newest&page=${offset}&begin_date=${lastWeekTodayString}&api-key=a8457610b68381085a3fff38d6a36337:6:74255139`)
+    return axios.get(`https://api.nytimes.com/svc/search/v2/articlesearch.json?q=${search}&sort=newest&page=${offset}&begin_date=${lastWeek}&api-key=a8457610b68381085a3fff38d6a36337:6:74255139`)
     .then(response => response.data);
   }
 };
