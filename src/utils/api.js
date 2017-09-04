4f2966fa3ce419d44414f3a68942966d9b55dad7
cooperbuilt <evanncooper@gmail.com>
2017-09-04 16:50:27 -0400

    Obscure key

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,25 +1,26 @@
 import axios from 'axios';
 import Moment from 'moment';
+import { key } from './secrets.js';
+
 
 export function getStories(desk, offset, search) {
   // Create a date object and roll it back 7 days
   // Then use that 7 day marker as the begin_date for article search
 
   const lastWeek = Moment().subtract(7, 'days').format('YYYYMMDD');
-  console.log(lastWeek);
 
   if (desk === '') {
-    return axios.get(`https://api.nytimes.com/svc/search/v2/articlesearch.json?fq=section:Today's Headlines&sort=newest&page=${offset}&begin_date=${lastWeek}&api-key=a8457610b68381085a3fff38d6a36337:6:74255139`)
+    return axios.get(`https://api.nytimes.com/svc/search/v2/articlesearch.json?fq=section:Today's Headlines&sort=newest&page=${offset}&begin_date=${lastWeek}&api-key=${key}`)
     .then(response => response.data);
   }
 
   if (arguments.length === 2) {
-    return axios.get(`https://api.nytimes.com/svc/search/v2/articlesearch.json?fq=news_desk:${desk}&sort=newest&page=${offset}&begin_date=${lastWeek}&api-key=a8457610b68381085a3fff38d6a36337:6:74255139`)
+    return axios.get(`https://api.nytimes.com/svc/search/v2/articlesearch.json?fq=news_desk:${desk}&sort=newest&page=${offset}&begin_date=${lastWeek}&api-key=${key}`)
       .then(response => response.data);
   }
 
   else {
-    return axios.get(`https://api.nytimes.com/svc/search/v2/articlesearch.json?q=${search}&sort=newest&page=${offset}&begin_date=${lastWeek}&api-key=a8457610b68381085a3fff38d6a36337:6:74255139`)
+    return axios.get(`https://api.nytimes.com/svc/search/v2/articlesearch.json?q=${search}&sort=newest&page=${offset}&begin_date=${lastWeek}&api-key=${key}`)
     .then(response => response.data);
   }
 };
